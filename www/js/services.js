888e607c2c104bd0dcf933e2da5a424a78ed1c0b
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2015-07-27 00:25:49 +0100

    Initial commit

---
+++ www/js/services.js
@@ -0,0 +1,160 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  },{
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+
+    .factory('Talks', function() {
+      // Might use a resource here that returns a JSON array
+
+      // Some fake testing data
+      var talks = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+      }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+      },{
+        id: 2,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+      }, {
+        id: 3,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+      }, {
+        id: 4,
+        name: 'Mike Harrington',
+        lastText: 'This is wicked good ice cream.',
+        face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+      }];
+
+      return {
+        all: function() {
+          return talks;
+        },
+        get: function(talkId) {
+          for (var i = 0; i < chats.length; i++) {
+            if (talks[i].id === parseInt(talkId)) {
+              return talks[i];
+            }
+          }
+          return null;
+        }
+      };
+    })
+
+
+
+
+
+
+
+.factory('LocationsService', [ function() {
+
+  var locationsObj = {};
+
+  locationsObj.savedLocations = [
+    {
+      name : "PyConUk, Coventry",
+      lat : 52.402956,
+      lng : -1.503969
+    },
+    {
+      name : "London, England",
+      lat : 52.402956,
+      lng : -0.126236
+    },
+    {
+      name : "Paris, France",
+      lat : 48.864716,
+      lng : 2.349014
+    },
+    {
+      name : "Moscow, Russia",
+      lat : 55.752121,
+      lng : 37.617664
+    },
+    {
+      name : "Rio de Janeiro, Brazil",
+      lat : -22.970722,
+      lng : -43.182365
+    },
+    {
+      name : "Sydney, Australia",
+      lat : -33.865143,
+      lng : 151.209900
+    }
+
+  ];
+
+  return locationsObj;
+
+}])
+
+.factory('InstructionsService', [ function() {
+
+  var instructionsObj = {};
+
+  instructionsObj.instructions = {
+    newLocations : {
+      text : 'To add a new location, tap and hold on the map',
+      seen : false
+    }
+  };
+
+  return instructionsObj;
+
+}]);
